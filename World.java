04d8f89ce553d21491ce07d1d8b76535890d39a4
ambb99 <amberlwilliams.02@gmail.com>
2024-11-06 00:33:44 -0600

    Update World.java

--- World.java
+++ World.java
@@ -3,40 +3,38 @@
 
 import java.util.Scanner;
 
-public class World{
+public class World {
 
-	private static String[][][] levels;
+    private static String[][][] levels;
 
-	public static void start(){
-		//Get all the map data and save it for later
-		Scanner input = new Scanner(System.in);
-		int count = input.nextInt();
+    public static void start() {
+        // Get all the map data and save it for later
+        Scanner input = new Scanner(System.in);
+        int count = input.nextInt();
+        levels = new String[count][][];
 
-		levels = new String[count][][];
+        for (int lvl = 0; lvl < count; lvl++) {
+            int rows = input.nextInt();
+            int cols = input.nextInt();
+            setLevel(lvl, rows, cols, input);
+        }
+    }
 
-		for (int lvl = 0; lvl < count; lvl++)
-			{
-				int rows = input.nextInt();
-				int cols = input.nextInt();
-				setLevel(lvl, rows, cols, input);
-			}
-	}
+    public static void setLevel(int lvl, int rows, int cols, Scanner input) {
+        levels[lvl] = new String[rows][cols];
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                String tile = input.next();
+                levels[lvl][y][x] = tile;
+            }
+        }
+    }
 
-	public static void setLevel(int lvl, int rows, int cols, Scanner input){
-		levels[lvl] = new String[rows][cols];
-		for (int y = 0; y < rows; y++)
-			{
-				for (int x = 0; x < cols; x++)
-					{
-						String tile = input.next();
-						levels[lvl][y][x] = tile;
-					}
-			}
-	}
+    public static String[][] getLevel(int level) {
+        return levels[level];
+    }
 
-	public static String[][] getLevel(int level){
-		return levels[level];
-	}
-
-
+    public static int getLength() {
+        return levels.length;
+    }
 }

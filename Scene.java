655eff7b07cc8c1e4bd8440bad4724b962066aab
ambb99 <amberlwilliams.02@gmail.com>
2024-11-05 23:52:37 -0600

    Update Scene.java

--- Scene.java
+++ Scene.java
@@ -1,71 +1,66 @@
 //Amber and Dom Ketchens
 //Project 4: Maze Game - Scene
 
-public class Scene{
-private static final int TILE_SIZE = 32;
+public class Scene {
+    private static final int TILE_SIZE = 32;
 
-	private static int rows;
-	private static int cols;
-	private static boolean[][] walls;
-	private static int width;
-	private static int height;
-	private static String floorImage;
-	private static String wallImage;
+    private static int rows;
+    private static int cols;
+    private static boolean[][] walls;
+    private static int width;
+    private static int height;
+    private static String floorImage;
+    private static String wallImage;
 
-	 public static void start(int level){
-	 	floorImage = "Assets/tile-passage.png";
-	 	wallImage = "Assets/tile-brickwall.png";
+    public static void start(int level) {
+        floorImage = "Assets/tile-passage.png";
+        wallImage = "Assets/tile-brickwall.png";
 
-	 	String[][] map = World.getLevel(level);
-	 	rows = map.length;
-	 	cols = map[0].length;
+        String[][] map = World.getLevel(level);
+        rows = map.length;
+        cols = map[0].length;
 
-	 	width = cols * TILE_SIZE;
-	 	height = rows * TILE_SIZE;
+        width = cols * TILE_SIZE;
+        height = rows * TILE_SIZE;
 
-	 	walls = new boolean[rows][cols];
-	 	for (int y = 0; y < rows; y++)
-	 		{
-	 			for (int x = 0; x < cols; x++)
-	 				{
-	 					String tile = map[y][x];
-	 					setTile(x, y, tile);
-	 				}
-	 		}
+        walls = new boolean[rows][cols];
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                String tile = map[y][x];
+                setTile(x, y, tile);
+            }
+        }
 
-	 	//Setup canvas data (tile & scale)
-	 	StdDraw.setCanvasSize(width, height);
-	 	StdDraw.setXscale(0.0, width);
-	 	StdDraw.setYscale(height, 0.0);
-	 }
+        // Setup canvas data (tile & scale)
+        StdDraw.setCanvasSize(width, height);
+        StdDraw.setXscale(0.0, width);
+        StdDraw.setYscale(height, 0.0);
+    }
 
-	 public static void setTile(int x, int y, String tile){
-	 	if (tile.equals("#") ){
-	 		walls[y][x] = true;
-	 	}
-	 	else if (tile.equals("@"))
-	 		{
-	 			Player.start(x, y);
-	 		}
-	 }
+    public static void setTile(int x, int y, String tile) {
+        if (tile.equals("#")) {
+            walls[y][x] = true;
+        } else if (tile.equals("@")) {
+            Player.start(x, y);
+        }
+    }
 
-	 public static void draw(){
-	 	for (int y = 0; y < rows; y++)
-	 		{
-	 			for (int x = 0; x < cols; x++)
-	 				{
-	 					int tileX = x * TILE_SIZE + TILE_SIZE/2;
-	 					int tileY = y * TILE_SIZE + TILE_SIZE/2;
+    public static void draw() {
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                int tileX = x * TILE_SIZE + TILE_SIZE / 2;
+                int tileY = y * TILE_SIZE + TILE_SIZE / 2;
 
-	 					if (walls[y][x] == true)
-	 						{
-	 							StdDraw.picture(tileX, tileY, wallImage);
-	 						}
-	 					else
-	 						{
-	 							StdDraw.picture(tileX, tileY, floorImage);
-	 						}
-	 				}
-	 		}
-	 }
+                if (walls[y][x] == true) {
+                    StdDraw.picture(tileX, tileY, wallImage);
+                } else {
+                    StdDraw.picture(tileX, tileY, floorImage);
+                }
+            }
+        }
+    }
+
+    public static boolean canMove(int x, int y) {
+        return !walls[y][x];
+    }
 }

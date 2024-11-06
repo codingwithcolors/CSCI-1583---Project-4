c7b4adb5707c58bad1aaf359c422bff87954d9f3
ambb99 <amberlwilliams.02@gmail.com>
2024-11-06 00:36:10 -0600

    Update Exit.java

--- Exit.java
+++ Exit.java
@@ -1,5 +1,5 @@
-//Amber and Dom Ketchens
-//Project 4: Maze Game - Exit
+// Amber and Dom Ketchens
+// Project 4: Maze Game - Exit
 
 public class Exit {
     public static final int TILE_SIZE = 32;
@@ -10,12 +10,20 @@ public class Exit {
     public static void start(int x, int y) {
         Exit.x = x;
         Exit.y = y;
-        image = "Assets/tile-exit.png";  // Path to the exit image
+        image = "Assets/tile-exit.png";
     }
 
     public static void draw() {
         int tileX = x * TILE_SIZE + TILE_SIZE / 2;
         int tileY = y * TILE_SIZE + TILE_SIZE / 2;
         StdDraw.picture(tileX, tileY, image);
+    }
+
+    public static int getX() {
+        return x;
+    }
+
+    public static int getY() {
+        return y;
     }
 }

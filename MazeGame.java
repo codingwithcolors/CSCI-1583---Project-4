afbb706e5a263d1b7b52ff7eba6206a7efbfc00a
ambb99 <amberlwilliams.02@gmail.com>
2024-11-10 16:40:02 -0600

    Update MazeGame.java

--- MazeGame.java
+++ MazeGame.java
@@ -1,8 +1,8 @@
 //Amber and Dom Ketchens
-//Project 4: MazeGame
+//Project 4: Maze Game
 
 public class MazeGame{
-    private static boolean gameOver;
+    private static boolean gameOver; // checks if game's over
 
         //Start game algorithm
         public static void start(){
@@ -10,23 +10,29 @@ public class MazeGame{
             level = 0;
             World.start();
             Scene.start(level);
+            // starting player positions
             Player.start(1,1);
             Ghost.start(5,5);
         }
 
+        // update game logic
         public static void update(){
-            Player.update();
-            Ghost.update();
+            Player.update(); // update player position
+            Ghost.update(); // update ghost position
             Scene.keyLogic(level); // Pass keyLogic
+
+            // check if player reaches exit and has the key
             if (Player.getX() == Exit.getX() && Player.getY() == Exit.getY() && Scene.hasKey()) {
                 System.out.println("Current level: " + level);
-                level++;
+                level++; // increment level
                 System.out.println("Player advanced to level: " + level);
+
+                // if player reached the last last, end the game
                 if (level == World.getLength()) {
                     gameOver = true;
                 }
                 else {
-                    Scene.start(level);
+                    Scene.start(level); // start new level scene
                 }
             }
             // checks if ghost catches player
@@ -35,6 +41,7 @@ public class MazeGame{
             }
         }
 
+        // renders (draw)
         public static void render(){
             Scene.draw(); // draw scene
             Exit.draw(); // draw exit
@@ -43,15 +50,16 @@ public class MazeGame{
             StdDraw.show(100);
         }
 
+        // main game loop
         public static void main(String[] args){
-            start();
-            while (gameOver == false)
+            start(); // starts game
+            while (gameOver == false) // loops while game isnt over
                 {
-                    update();
-                    render();
+                    update(); // updates logic
+                    render(); // renders updated game
                 }
 
         }
 
-        public static int level;
+        public static int level; // current game level
 }
